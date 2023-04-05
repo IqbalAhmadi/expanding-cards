500a5f47f30cdb5d64df783f316e0df7367f6087
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2023-04-05 02:07:15 -0700

    added js code

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+// first, we need to bring in all the cards
+const panels = document.querySelectorAll('.panel')
 
+// loop through each
+panels.forEach((panel) => {
+  // adding eventlistener for a click
+  panel.addEventListener('click', () => {
+    removeActiveClasses()
+    panel.classList.add('active')
+  })
+})
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove('active')
+  })
+}
